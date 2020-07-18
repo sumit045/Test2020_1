15d1bd213c0d73c0075467b14ea5923533f076f0
sumit045 <sumit619.hellboy@gmail.com>
2020-07-19 01:25:34 +0530

    First Commit

---
+++ Test2020_A_4/src/main/java/Test2020_4/Test2020_A_4/jobSchedular.java
@@ -0,0 +1,19 @@
+package Test2020_4.Test2020_A_4;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class jobSchedular {
+
+	@Test
+	public void test()
+	{
+		System.out.println("Testing");
+		String path = "C:\\Users\\SumitKumar\\Documents\\Anith\\Test2020_A_4\\Drivers\\geckodriver.exe";
+		System.setProperty("webdriver.gecko.driver", path);
+		
+		WebDriver driver = new FirefoxDriver();
+	}
+	
+}
